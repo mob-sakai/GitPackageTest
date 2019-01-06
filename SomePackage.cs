f6612f97e9eb2398207e7a817488c14585f21d12
mob-sakai <sakai861104@gmail.com>
2019-01-06 21:37:10 +0900

    core b 0.1.0

---
+++ SomePackage.cs
@@ -0,0 +1,16 @@
+﻿namespace Coffee.CoreB
+{
+	public class Util
+	{
+		public static int Sub (int a, int b)
+		{
+			return a - b;
+		}
+
+		[UnityEditor.InitializeOnLoadMethod]
+		public static void InitializeOnLoadMethod ()
+		{
+			UnityEngine.Debug.Log ("Coffee.CoreB.Util: InitializeOnLoadMethod");
+		}
+	}
+}
