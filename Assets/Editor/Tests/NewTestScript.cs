c70cfa3a845d17e5e2827a743a53044dfd651992
mob-sakai <sakai861104@gmail.com>
2019-01-07 20:24:26 +0900

    add test

--- Assets/Editor/Tests/NewTestScript.cs
+++ Assets/Editor/Tests/NewTestScript.cs
@@ -12,6 +12,7 @@ namespace Tests
         [Test]
         public void NewTestScriptSimplePasses()
         {
+            Assert.IsTrue(false);
             // Use the Assert class to test conditions
         }
 
