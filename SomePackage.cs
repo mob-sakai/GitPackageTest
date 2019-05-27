ad2b0faf1910feca59e7fa6f527e264718c07bfc
mob-sakai <sakai@mobcast.jp>
2019-05-27 15:35:49 +0900

    core b 0.3.0

--- SomePackage.cs
+++ SomePackage.cs
@@ -13,14 +13,14 @@
 			UnityEngine.Debug.Log ("Coffee.CoreB.Util: InitializeOnLoadMethod");
 
 #if COM_UNITY_TEXTMESHPRO
-			Debug.Log ("[Coffee.CoreB] <color=green>a symbol 'COM_UNITY_TEXTMESHPRO' is defined.</color>");
+			UnityEngine.Debug.Log ("[Coffee.CoreB] <color=green>a symbol 'COM_UNITY_TEXTMESHPRO' is defined.</color>");
 #else
-			Debug.Log ("[Coffee.CoreB] <color=orange>a symbol 'COM_UNITY_TEXTMESHPRO' is not defined.</color>");
+			UnityEngine.Debug.Log ("[Coffee.CoreB] <color=orange>a symbol 'COM_UNITY_TEXTMESHPRO' is not defined.</color>");
 #endif
 #if COM_UNITY_QUICKSEARCH
-			Debug.Log ("[Coffee.CoreB] <color=green>a symbol 'COM_UNITY_QUICKSEARCH' is defined.</color>");
+			UnityEngine.Debug.Log ("[Coffee.CoreB] <color=green>a symbol 'COM_UNITY_QUICKSEARCH' is defined.</color>");
 #else
-			Debug.Log ("[Coffee.CoreB] <color=orange>a symbol 'COM_UNITY_QUICKSEARCH' is not defined.</color>");
+			UnityEngine.Debug.Log ("[Coffee.CoreB] <color=orange>a symbol 'COM_UNITY_QUICKSEARCH' is not defined.</color>");
 #endif
 		}
 	}
