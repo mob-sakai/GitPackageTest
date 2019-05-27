c1a93ed56e7febf6c035dc8d89796e12ee941873
mob-sakai <sakai@mobcast.jp>
2019-05-27 15:20:14 +0900

    core b 0.2.0

--- SomePackage.cs
+++ SomePackage.cs
@@ -11,6 +11,17 @@
 		public static void InitializeOnLoadMethod ()
 		{
 			UnityEngine.Debug.Log ("Coffee.CoreB.Util: InitializeOnLoadMethod");
+
+#if COM_UNITY_TEXTMESHPRO
+			Debug.Log ("[Coffee.CoreB] <color=green>a symbol 'COM_UNITY_TEXTMESHPRO' is defined.</color>");
+#else
+			Debug.Log ("[Coffee.CoreB] <color=orange>a symbol 'COM_UNITY_TEXTMESHPRO' is not defined.</color>");
+#endif
+#if COM_UNITY_QUICKSEARCH
+			Debug.Log ("[Coffee.CoreB] <color=green>a symbol 'COM_UNITY_QUICKSEARCH' is defined.</color>");
+#else
+			Debug.Log ("[Coffee.CoreB] <color=orange>a symbol 'COM_UNITY_QUICKSEARCH' is not defined.</color>");
+#endif
 		}
 	}
 }
